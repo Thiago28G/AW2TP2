09ff5f0f0e27a5c19cafbdaac713dbd936a3d52d
Thiago <thiagogallegodelfino@hotmail.com>
2026-05-18 14:12:49 -0300

    feat: agregar CORS para cliente en puerto 5500

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import productosRouter from './routes/productos.js';
 import usuariosRouter from './routes/usuarios.js';
 import ventasRouter from './routes/ventas.js';
@@ -6,6 +7,9 @@ import ventasRouter from './routes/ventas.js';
 const app = express();
 const PORT = 3000;
 
+app.use(cors({
+    origin: ['http://localhost:5500', 'http://127.0.0.1:5500']
+}));
 app.use(express.json());
 
 app.use('/api/productos', productosRouter);
